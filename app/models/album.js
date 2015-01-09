50d4c35d90eed2d7d3a16c1a161f398c50b00c40
Jiangyue Zhu <zhujy8833@gmail.com>
2015-01-09 14:11:33 -0800

    computed properties - totalDuration, songCount

--- app/models/album.js
+++ app/models/album.js
@@ -6,5 +6,15 @@ export default DS.Model.extend({
     name : DS.attr(),
     artist : DS.attr(),
     isExplicit : DS.attr('boolean', {defaultValue : false}),
-    songs : DS.hasMany('song')
+    songs : DS.hasMany('song'),
+
+    songCount : function(){
+        return this.get('songs.length')
+    }.property('songs.length'),
+
+    totalDuration : function() {
+        return this.get('songs').reduce(function(prevValue, song) {
+            return prevValue + song.get('duration');
+        }, 0);
+    }.property('songs.@each.duration')
 });
