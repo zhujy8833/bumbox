26e11665bc3f62d94941cf80e84b6ddfb858abc2
Jiangyue Zhu <zhujy8833@gmail.com>
2015-01-10 10:58:22 -0800

    audio player service

--- app/components/song-row.js
+++ app/components/song-row.js
@@ -1,15 +1,20 @@
 
 import Ember from 'ember';
+var inject = Ember.inject;
 
 export default Ember.Component.extend({
     isPlaying : false,
     tagName : "tr",
+
+    player : inject.service(),
     actions : {
         pause : function() {
+            this.get('player').pause(this.get('song'));
             this.set('isPlaying', false);
         },
 
         play : function() {
+            this.get('player').play(this.get('song'));
             this.set('isPlaying', true);
         }
     }
